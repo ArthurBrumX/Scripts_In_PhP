822a35a4dcba1c249b6c837d8ddb61ea78ec2b46
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-09-17 04:57:04 -0300

    up

---
+++ poo/Aula-001/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 001</title>
+</head>
+<body>
+    <?php
+        require_once 'classe_001.php';
+        $c1 = new caneta; // instanciamento
+        $c1 -> cor = "Azul";
+        $c1 -> ponta = 0.5;
+        $c1 -> tampada = false
+
+    ?>
+</body>
+</html>
