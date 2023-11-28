2673722c038543ad9651eb970e823e2c4bfc9c21
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-11-28 12:27:38 -0300

    up

---
+++ vagas/index.php
@@ -0,0 +1,5 @@
+<?php
+   include __DIR__.'/includes/header.php'; // Ali é um ponto e tem um espaço entre o include e o DIR
+   include __DIR__.'/includes/listagem.php';
+   include __DIR__.'/includes/footer.php';
+?>
