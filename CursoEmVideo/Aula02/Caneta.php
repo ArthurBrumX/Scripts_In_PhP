fe7f37461d0216dc141a8890f49ba5a47aa7cd10
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2024-01-03 03:44:24 -0400

    up

---
+++ CursoEmVideo/Aula02/Caneta.php
@@ -0,0 +1,18 @@
+<?php
+class Caneta {
+    var $modelo; // Variavel
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampada;
+
+    function rabiscar(){ // criando uma funcao
+
+    }
+    function tampar(){
+
+    }
+    function destampar(){
+        
+    }
+}
